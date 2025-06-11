659860e4ebeefdb83f0b638b5543e15617b74a2e
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-11 22:24:25 +0500

    Create FindMissingNumberInArray.java

---
+++ FindMissingNumber/FindMissingNumberInArray.java
@@ -0,0 +1,17 @@
+
+class FindMissingNumberInArray
+{
+	public static void main(String [] args)
+	{
+		 int[] a={1,2,3,4,5,6,7,9,10};
+		
+	   int ExpectedNumberOfElements=a.length+1;
+		 int ExpectedTotalSum=ExpectedNumberOfElements*(ExpectedNumberOfElements+1)/2;
+		 int actualSum=0;
+		 for(int i=0;i<a.length;i++)
+		 {
+			 actualSum=actualSum+a[i];
+		 }
+		 System.out.println("Missing number is: "+(ExpectedTotalSum-actualSum));
+	 }
+}
