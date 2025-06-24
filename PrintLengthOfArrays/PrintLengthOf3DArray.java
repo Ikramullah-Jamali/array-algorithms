2e4f02b1b1ad8b581f8221cadeb4ba98d671b1a7
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-24 22:31:14 +0500

    Add files via upload

---
+++ PrintLengthOfArrays/PrintLengthOf3DArray.java
@@ -0,0 +1,10 @@
+
+
+class PrintLengthOf3DArray
+{
+    public static void main(String [] args)
+    {
+        int [][][] a={{{10,20},{30,40,50,60},{70,80,90}}};
+        System.out.println("Length of the array: " + a[0][0].length);
+    }
+}
