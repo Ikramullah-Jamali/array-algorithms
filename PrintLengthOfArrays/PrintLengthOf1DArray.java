597dd311d26853acf94c917fff27ef059dd2b379
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-24 22:30:07 +0500

    Create PrintLengthOf1DArray.java

---
+++ PrintLengthOfArrays/PrintLengthOf1DArray.java
@@ -0,0 +1,11 @@
+
+// Program to print the length of a 1D array
+
+class PrintLengthOf1DArray 
+{
+    public static void main(String[] args) 
+    {
+        int[] a = {70, 80, 90};
+        System.out.print("Length of the array: " + a.length);
+    }
+}
