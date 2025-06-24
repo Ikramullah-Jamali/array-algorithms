8633afa165ce57ad58d986bf4106914c4e94daf2
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-24 22:30:32 +0500

    Add files via upload

---
+++ PrintLengthOfArrays/PrintLengthOf2DArray.java
@@ -0,0 +1,10 @@
+
+
+class PrintLengthOf2DArray   
+{
+    public static void main(String [] args)
+   {
+      int [][] a={{10,20,30,40},{50,60},{70,80,90}};
+      System.out.print("Length of the array: " + a[0].length);
+   }
+}
