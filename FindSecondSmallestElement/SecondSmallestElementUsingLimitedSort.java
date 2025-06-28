bd74e0316043a768e31f4d03d21ef00b213c98aa
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-28 13:55:48 +0500

    Add files via upload

---
+++ FindSecondSmallestElement/SecondSmallestElementUsingLimitedSort.java
@@ -0,0 +1,39 @@
+
+class SecondSmallestElementUsingLimitedSort
+{
+	public static void main(String [] args)
+	{
+		int [] a={11,13,9,15,4,7};
+		int temp;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+			    }
+			}
+			if(i==1)  // Stop after sorting the top 2 smallest elements
+			{
+			    break;
+			}
+		}
+		// Print partially sorted array
+        System.out.print("Partially sorted array (top 2 smallest elements): ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+"  ");
+		}
+		if (a.length >= 2)
+        {
+            System.out.println("\nSecond smallest number in array is: " + a[1]);
+        }
+        else
+        {
+            System.out.println("\nArray does not have a second smallest element.");
+        }
+	}
+}	
