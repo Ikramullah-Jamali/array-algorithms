3b2c9bf165d12f4b9b1374170c815580d86e3f52
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2025-06-28 13:25:31 +0500

    Create SecondLargestElementUsingBubbleSort.java

---
+++ FindSecondLargestElement/SecondLargestElementUsingBubbleSort.java
@@ -0,0 +1,36 @@
+
+class SecondLargestElementUsingBubbleSort 
+{
+	public static void main(String [] args)
+	{
+		int [] a={11,6,9,15,4,7};
+		int temp;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]<a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+			    }
+			}
+		}
+		// Print the sorted array
+        System.out.print("Sorted array in descending order: ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+"  ");
+		}
+		if (a.length >= 2) 
+		{
+        System.out.println("\nSecond largest number in array is: " + a[1]);
+    } 
+    else 
+		{
+        System.out.println("\nArray does not have a second largest number.");
+    }
+		
+	}
+}	
